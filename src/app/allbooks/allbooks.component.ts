9aecc17063aa07642e45990a38a421d6a8591fa6
AishuPola <aishwarya.pola@proclink.com>
2024-08-04 12:12:51 +0530

    components created

---
+++ src/app/allbooks/allbooks.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-allbooks',
+  standalone: true,
+  imports: [],
+  templateUrl: './allbooks.component.html',
+  styleUrl: './allbooks.component.scss'
+})
+export class AllbooksComponent {
+
+}
